36a36a52bd4266d29040a2d4ccd4f2c247b49a1c
Joshua Corpin <joshua.corpin@lgusuite.com>
2021-10-07 12:16:24 +0800

    Initial commit

---
+++ utils/security/index.js
@@ -0,0 +1,15 @@
+const allowedOrigins = [
+  'http://localhost:4200',
+  'http://localhost:4201',
+  'http://localhost',
+];
+
+exports.origin = function (origin, callback) {
+  if (!origin) return callback(null, true);
+
+  if (allowedOrigins.indexOf(origin) === -1) {
+    const message = `This site ${origin} doesn't have a permission to access this site.`;
+    return callback(new AppError(message, 403), false);
+  }
+  return callback(null, true);
+};
