4ea03e689c36ecc1e94adabeb1355e732d32bfd0
Joshua Corpin <joshua.corpin@lgusuite.com>
2021-10-08 11:49:35 +0800

    Added type params to route

--- routes/GENERAL/auth.routes.js
+++ routes/GENERAL/auth.routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const authController = require('../../controllers/GENERAL/auth.controller');
 
-router.post('/login', authController.login);
+router.post('/login/:type', authController.login);
 
 module.exports = router;
