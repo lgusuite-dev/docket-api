18707d2c3445f618d3e9eaa0330eafd8aad8bf93
Joshua Corpin <joshua.corpin@lgusuite.com>
2021-10-08 11:50:39 +0800

    Refactored route handlers

--- controllers/GENERAL/user.controller.js
+++ controllers/GENERAL/user.controller.js
@@ -10,6 +10,65 @@ const QueryFeatures = require('../../utils/query/queryFeatures');
 
 const createID = () => new mongoose.Types.ObjectId();
 
+const generateGetUsersQuery = (type, req) => {
+  if (type === 'admins') return { status: { $ne: 'Deleted' }, type: 'Admin' };
+
+  if (type === 'users')
+    return {
+      status: { $ne: 'Deleted' },
+      type: 'User',
+      _tenantId: req.user._tenantId,
+    };
+};
+
+const generateGetOneUserQuery = (type, req) => {
+  const { id } = req.params;
+
+  const query = { _id: id, status: { $ne: 'Deleted' } };
+
+  if (type === 'admins') query.type = 'Admin';
+
+  if (type === 'users') {
+    query.type = 'User';
+    query._tenantId = req.user._tenantId;
+  }
+
+  return query;
+};
+
+const generateUserData = (type, req) => {
+  const pickFields = [
+    'firstName',
+    'lastName',
+    'middleName',
+    'email',
+    'mobileNumber',
+  ];
+
+  const filteredBody = _.pick(req.body, pickFields);
+  const randomPassword = _generateRandomPassword();
+  const _id = createID();
+
+  if (type === 'admins') {
+    filteredBody.password = randomPassword;
+    filteredBody.passwordConfirm = randomPassword;
+    filteredBody._id = _id;
+    filteredBody._tenantId = _id;
+    filteredBody._createdBy = req.user._id;
+  }
+
+  if (type === 'users') {
+    filteredBody.type = 'User';
+    filteredBody.password = randomPassword;
+    filteredBody.passwordConfirm = randomPassword;
+    filteredBody._id = _id;
+    filteredBody._createdBy = req.user._id;
+    filteredBody._tenantId = req.user._tenantId;
+  }
+
+  return filteredBody;
+};
+
 exports.createSuper = catchAsync(async (req, res, next) => {
   if (!req.headers.supertoken || !req.headers.superuser)
     return next(new AppError('Forbidden', 403));
@@ -39,117 +98,33 @@ exports.createSuper = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createAdmin = catchAsync(async (req, res, next) => {
-  const pickFields = [
-    'firstName',
-    'lastName',
-    'middleName',
-    'email',
-    'mobileNumber',
-  ];
-  const filteredBody = _.pick(req.body, pickFields);
-  const randomPassword = _generateRandomPassword(8);
-  const _id = createID();
-
-  filteredBody.password = randomPassword;
-  filteredBody.passwordConfirm = randomPassword;
-  filteredBody._id = _id;
-  filteredBody._tenantId = _id;
-  filteredBody._createdBy = req.user._id;
-
-  const newAdmin = await User.create(filteredBody);
-
-  res.status(201).json({
-    status: 'success',
-    env: {
-      user: newAdmin,
-    },
-  });
-});
-
 exports.createUser = catchAsync(async (req, res, next) => {
-  const pickFields = [
-    'firstName',
-    'lastName',
-    'middleName',
-    'email',
-    'mobileNumber',
-  ];
-  const filteredBody = _.pick(req.body, pickFields);
-  const randomPassword = _generateRandomPassword(8);
-  const _id = createID();
+  const endpoint = req.originalUrl;
+  const validTypes = ['admins', 'users'];
+  const type = endpoint.split('/api/v1/tenants/')[1].split('?')[0];
+  const userData = generateUserData(type, req);
 
-  filteredBody.type = 'User';
-  filteredBody.password = randomPassword;
-  filteredBody.passwordConfirm = randomPassword;
-  filteredBody._id = _id;
-  filteredBody._tenantId = req.user._id;
-  filteredBody._createdBy = req.user._id;
+  if (!validTypes.includes(type))
+    return next(new AppError('Invalid type params', 400));
 
-  const newAdmin = await User.create(filteredBody);
+  const newUser = await User.create(userData);
 
   res.status(201).json({
     status: 'success',
     env: {
-      user: newAdmin,
-    },
-  });
-});
-
-exports.getAllAdmins = catchAsync(async (req, res, next) => {
-  const initialQuery = { status: { $ne: 'Deleted' }, type: 'Admin' };
-
-  const queryFeature = new QueryFeatures(User.find(initialQuery), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate()
-    .populate();
-
-  const nQueryFeature = new QueryFeatures(User.find(initialQuery), req.query)
-    .filter()
-    .count();
-
-  const admins = await queryFeature.query;
-  const nAdmins = await nQueryFeature.query;
-
-  res.status(200).json({
-    status: 'success',
-    total_docs: nAdmins,
-    results: admins.length,
-    env: {
-      admins,
-    },
-  });
-});
-
-exports.getAdmin = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-
-  const intialQuery = { _id: id, status: { $ne: 'Deleted' }, type: 'Admin' };
-
-  const queryFeature = new QueryFeatures(User.findOne(intialQuery), req.query)
-    .limitFields()
-    .populate();
-
-  const user = await queryFeature.query;
-
-  if (!user) return next(new AppError('User not found', 404));
-
-  res.status(200).json({
-    status: 'success',
-    env: {
-      user,
+      user: newUser,
     },
   });
 });
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const initialQuery = {
-    status: { $ne: 'Deleted' },
-    type: 'User',
-    _tenantId: req.user._tenantId,
-  };
+  const endpoint = req.originalUrl;
+  const validTypes = ['admins', 'users'];
+  const type = endpoint.split('/api/v1/tenants/')[1].split('?')[0];
+  const initialQuery = generateGetUsersQuery(type, req);
+
+  if (!validTypes.includes(type))
+    return next(new AppError('Invalid type params', 400));
 
   const queryFeature = new QueryFeatures(User.find(initialQuery), req.query)
     .filter()
@@ -176,14 +151,13 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
 });
 
 exports.getUser = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
+  const endpoint = req.originalUrl;
+  const validTypes = ['admins', 'users'];
+  const type = endpoint.split('/api/v1/tenants/')[1].split('/')[0];
+  const initialQuery = generateGetOneUserQuery(type, req);
 
-  const initialQuery = {
-    _id: id,
-    status: { $ne: 'Deleted' },
-    type: 'User',
-    _tenantId: req.user._tenantId,
-  };
+  if (!validTypes.includes(type))
+    return next(new AppError('Invalid type params', 400));
 
   const queryFeature = new QueryFeatures(User.findOne(initialQuery), req.query)
     .limitFields()
@@ -201,33 +175,14 @@ exports.getUser = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteAdmin = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-
-  const initialQuery = {
-    _id: id,
-    status: { $ne: 'Deleted' },
-    type: 'Admin',
-  };
-
-  const user = await User.findOneAndUpdate(initialQuery, { status: 'Deleted' });
-
-  if (!user) return next(new AppError('User not found', 404));
-
-  res.status(204).json({
-    status: 'success',
-  });
-});
-
 exports.deleteUser = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
+  const endpoint = req.originalUrl;
+  const validTypes = ['admins', 'users'];
+  const type = endpoint.split('/api/v1/tenants/')[1].split('/')[0];
+  const initialQuery = generateGetOneUserQuery(type, req);
 
-  const initialQuery = {
-    _id: id,
-    status: { $ne: 'Deleted' },
-    type: 'User',
-    _tenantId: req.user._tenantId,
-  };
+  if (!validTypes.includes(type))
+    return next(new AppError('Invalid type params', 400));
 
   const user = await User.findOneAndUpdate(initialQuery, { status: 'Deleted' });
 
