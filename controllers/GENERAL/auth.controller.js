45a5aa1af2e5f5359ebca5179c0932ee7746544a
Joshua Corpin <joshua.corpin@lgusuite.com>
2021-10-08 11:49:07 +0800

    Changed implementation of login route handler: Handled super, admin, user login

--- controllers/GENERAL/auth.controller.js
+++ controllers/GENERAL/auth.controller.js
@@ -8,21 +8,33 @@ const {
   _createSessionToken,
 } = require('../../utils/tokens');
 
+const generateLoginQuery = (type) => {
+  if (type === 'super')
+    return { type: 'Superadmin', status: { $ne: 'Deleted' } };
+  if (type === 'admin') return { type: 'Admin', status: { $ne: 'Deleted' } };
+  if (type === 'user') return { type: 'User', status: { $ne: 'Deleted' } };
+};
+
 exports.login = catchAsync(async (req, res, next) => {
+  const { type } = req.params;
   const { email, password } = req.body;
+  const validTypes = ['super', 'admin', 'user'];
 
   if (!email) return next(new AppError('Please provide email', 400));
   if (!password) return next(new AppError('Please provide password', 400));
 
-  const user = await User.findOne({ email, status: { $ne: 'Deleted' } }).select(
-    '+password'
-  );
+  if (!validTypes.includes(type))
+    return next(new AppError('Invalid type params', 400));
 
-  if (user.status === 'Suspended')
-    return next(new AppError('Your Account is Suspended', 403));
+  const query = { ...generateLoginQuery(type), email };
+
+  const user = await User.findOne(query).select('+password');
 
   if (!user || !(await user.isPasswordCorrect(password, user.password)))
     return next(new AppError('Incorrect email or password', 401));
+
+  if (user.status === 'Suspended')
+    return next(new AppError('Your Account is Suspended', 403));
 
   const token = _signToken({ id: user._id });
   const session_token = _createSessionToken(user, token);
