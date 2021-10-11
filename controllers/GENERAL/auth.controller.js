dc48fb452ecd3c4b29f195c6496aedefeb5e6c5f
Joshua Corpin <joshua.corpin@lgusuite.com>
2021-10-11 10:05:15 +0800

    Added update info and update password route handler

--- controllers/GENERAL/auth.controller.js
+++ controllers/GENERAL/auth.controller.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const User = require('../../models/GENERAL/user.model');
 
 const catchAsync = require('../../utils/errors/catchAsync');
@@ -13,6 +15,22 @@ const generateLoginQuery = (type) => {
     return { type: 'Superadmin', status: { $ne: 'Deleted' } };
   if (type === 'admin') return { type: 'Admin', status: { $ne: 'Deleted' } };
   if (type === 'user') return { type: 'User', status: { $ne: 'Deleted' } };
+};
+
+const sendAuthResponse = (user, statusCode, res) => {
+  const token = _signToken({ id: user._id });
+  const session_token = _createSessionToken(user, token);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    session_token,
+    token,
+    env: {
+      user,
+    },
+  });
 };
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -36,19 +54,7 @@ exports.login = catchAsync(async (req, res, next) => {
   if (user.status === 'Suspended')
     return next(new AppError('Your Account is Suspended', 403));
 
-  const token = _signToken({ id: user._id });
-  const session_token = _createSessionToken(user, token);
-
-  user.password = undefined;
-
-  res.status(200).json({
-    status: 'success',
-    session_token,
-    token,
-    env: {
-      user,
-    },
-  });
+  sendAuthResponse(user, 200, res);
 });
 
 exports.authenticate = catchAsync(async (req, res, next) => {
@@ -90,3 +96,42 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.updateInfo = catchAsync(async (req, res, next) => {
+  const pickField = ['firstName', 'lastName', 'middleName', 'mobileNumber'];
+  const filteredBody = _.pick(req.body, pickField);
+
+  const user = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!user) return next(new AppError('User not found', 404));
+
+  sendAuthResponse(user, 200, res);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  const { currentPassword, confirmNewPassword, newPassword } = req.body;
+
+  if (!currentPassword)
+    return next(new AppError('Please provide current password', 400));
+
+  if (!newPassword)
+    return next(new AppError('Please provide new password', 400));
+
+  if (!confirmNewPassword)
+    return next(new AppError('Please confirm new password', 400));
+
+  const user = await User.findById(req.user._id).select('+password');
+
+  if (!user || !(await user.isPasswordCorrect(currentPassword, user.password)))
+    return next(new AppError('Incorrect current password', 400));
+
+  user.password = newPassword;
+  user.passwordConfirm = confirmNewPassword;
+
+  await user.save();
+
+  sendAuthResponse(user, 200, res);
+});
