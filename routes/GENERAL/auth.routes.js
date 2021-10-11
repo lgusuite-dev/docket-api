91ca610a2a84b7fbe21617a0fb9265f48fb910be
Joshua Corpin <joshua.corpin@lgusuite.com>
2021-10-11 10:05:52 +0800

    Added route for updating info and password

--- routes/GENERAL/auth.routes.js
+++ routes/GENERAL/auth.routes.js
@@ -4,4 +4,9 @@ const authController = require('../../controllers/GENERAL/auth.controller');
 
 router.post('/login/:type', authController.login);
 
+router.use(authController.authenticate);
+
+router.put('/update-info', authController.updateInfo);
+router.put('/update-password', authController.updatePassword);
+
 module.exports = router;
