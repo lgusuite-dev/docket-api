ad52e45c688e1ddf7bd2233044b5127b785e61b0
Joshua Corpin <joshua.corpin@lgusuite.com>
2021-10-18 17:00:52 +0800

    Added logout, and deleteuserapi

--- tests/utils/response/index.js
+++ tests/utils/response/index.js
@@ -2,6 +2,7 @@ const supertest = require('supertest');
 const app = require('../../../app');
 const request = supertest(app);
 
+// AUTH
 exports.login = (data, type) =>
   request.post(`/api/v1/auth/login/${type}`).send(data);
 
@@ -30,10 +31,17 @@ exports.verifyRPToken = (token) =>
 
 exports.getMe = (token = '', session_token = '') =>
   request
-    .get('/me')
+    .get('/api/v1/auth/me')
     .set('Authorization', `Bearer ${token}`)
     .set('s_auth', session_token);
 
+exports.logout = (token = '', session_token = '') =>
+  request
+    .get('/api/v1/auth/logout')
+    .set('Authorization', `Bearer ${token}`)
+    .set('s_auth', session_token);
+
+// USERS
 exports.createUserAPI = (data, type, token = '', session_token = '') =>
   request
     .post(`/api/v1/tenants/${type}`)
@@ -45,5 +53,11 @@ exports.updateUserAPI = (data, type, id, token = '', session_token = '') =>
   request
     .put(`/api/v1/tenants/${type}/${id}`)
     .send(data)
+    .set('Authorization', `Bearer ${token}`)
+    .set('s_auth', session_token);
+
+exports.deleteUserAPI = (id, type, token = '', session_token = '') =>
+  request
+    .patch(`/api/v1/${type}/${id}`)
     .set('Authorization', `Bearer ${token}`)
     .set('s_auth', session_token);
