1fb1cb3f4edd711c047f96f682f975cc69f816eb
LAPTOP-QNICKIJJ\LITZEN <nico@lgusuite.com>
2021-11-15 14:34:02 +0800

    added patch delete get in task routes & added patch in task controller

--- controllers/GENERAL/task.controller.js
+++ controllers/GENERAL/task.controller.js
@@ -239,7 +239,7 @@ exports.updateTaskStatus = catchAsync(async (req, res, next) => {
   const allowedStatus = [
     'Pending',
     'Completed',
-    'Deleted',
+    'Canceled',
     'Declined',
     'For Approval',
   ];
