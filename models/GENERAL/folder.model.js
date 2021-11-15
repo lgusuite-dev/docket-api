3e9870d663da88dd4dd17fc6c5f60b2f8ea76dfa
Joshua Corpin <jkcorpin@gmail.com>
2021-11-15 11:44:31 +0800

    Cleanup

--- models/GENERAL/folder.model.js
+++ models/GENERAL/folder.model.js
@@ -1,27 +1,27 @@
 const mongoose = require('mongoose');
 const FolderSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        _parentId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Folder'
-        },
-        _createdBy: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        status: {
-            type: String,
-            enum: ['Active', 'Deleted'],
-            default: 'Active'
-        }
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide folder name'],
     },
-    {
-        timestamps: true
-    }
-)
+    _parentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Folder',
+    },
+    _createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    status: {
+      type: String,
+      enum: ['Active', 'Deleted'],
+      default: 'Active',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 const Folder = mongoose.model('Folder', FolderSchema);
 module.exports = Folder;
