641695f402f7f69de35652c977142cfd3a4af273
Joshua Corpin <jkcorpin@gmail.com>
2021-11-23 15:16:39 +0800

    Added authenticate middleware

--- routes/GENERAL/scanned_document.routes.js
+++ routes/GENERAL/scanned_document.routes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
+const authController = require('../../controllers/GENERAL/auth.controller');
 const scannedDocumentController = require('../../controllers/GENERAL/scanned_document.controller');
+
+router.use(authController.authenticate);
 
 router.get('/', scannedDocumentController.searchDocument);
 
