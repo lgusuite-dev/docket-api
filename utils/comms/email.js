a1de0e1661f5aa00d0367ca40afca4a7b74751b3
Joshua Corpin <jkcorpin@gmail.com>
2021-12-17 14:07:17 +0800

    Added try catch block

--- utils/comms/email.js
+++ utils/comms/email.js
@@ -1,15 +1,20 @@
 const sgMail = require('@sendgrid/mail');
 
 exports.sendMail = async ({ to, subject, html, attachments = [] }) => {
-  const mailOptions = {
-    from: process.env.FROM_MAIL,
-    to,
-    subject,
-    html,
-    attachments,
-  };
+  try {
+    const mailOptions = {
+      from: process.env.FROM_MAIL,
+      to,
+      subject,
+      html,
+      attachments,
+    };
 
-  if (!attachments.length) delete mailOptions.attachments;
+    if (!attachments.length) delete mailOptions.attachments;
 
-  await sgMail.send(mailOptions);
+    await sgMail.send(mailOptions);
+  } catch (err) {
+    console.error(err);
+    console.error(err.message);
+  }
 };
