59b7f3b6c4567e88d564324d8ca4c8fa7d3a5162
LAPTOP-7VFOS1TK\rainraqui <rainier@lgusuite.com>
2022-03-14 12:34:33 +0800

    revert

--- utils/security/index.js
+++ utils/security/index.js
@@ -2,7 +2,7 @@ const AppError = require('../../utils/errors/AppError');
 
 const allowedOrigins = [
   'https://docket-ph.herokuapp.com',
-  'https://docket-ph-production.herokuapp.com',
+  'https://docket-ph-development.herokuapp.com',
   'https://docket-batac.herokuapp.com',
   'https://www.lgudocket.com',
   'http://localhost:4200',
