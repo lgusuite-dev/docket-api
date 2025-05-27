e4417b4db05a67909443d6f8cc544d2513ed0603
ncatilo <nap.catilo@codevelopr.com>
2025-05-27 18:21:18 +0800

    updated for aws deploy

--- start-tunnel.sh
+++ start-tunnel.sh
@@ -6,8 +6,8 @@ echo "✅ Tunnel is ready. Starting Node.js app..."
 # Debug mode check
 if [ "$NODE_DEBUG_MODE" = "true" ]; then
   echo "🛠 Starting Node in debug mode"
-  node --inspect=0.0.0.0:9229 src/server.js
+  node --inspect=0.0.0.0:9229 server.js
 else
   echo "🚀 Starting Node normally"
-  node src/server.js
+  node server.js
 fi
